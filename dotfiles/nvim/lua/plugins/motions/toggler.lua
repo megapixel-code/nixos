b249c3b5dfa4a06342b54d3f12e80829342dc5e8
megapixel-code <chainemegapixel@gmail.com>
2026-04-30 09:17:02 +0200

    feat(nvim/toggler): added toggler plugin

---
+++ dotfiles/nvim/lua/plugins/motions/toggler.lua
@@ -0,0 +1,16 @@
+return {
+   "nguyenvukhang/nvim-toggler",
+
+   init = function()
+      vim.keymap.set( "n", "<leader>tt", require( "nvim-toggler" ).toggle, { desc = "toggle word under cursor" } );
+   end,
+   opts = {
+      inverses = {
+         ["true"] = "false",
+         ["always"] = "never",
+         ["yes"] = "no",
+      },
+      remove_default_inverses = true,
+      remove_default_keybinds = true,
+   },
+};
