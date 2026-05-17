88d2cec2fd44b22d5ddf3db7eeec917d8a6f3f05
megapixel-code <chainemegapixel@gmail.com>
2026-05-17 11:36:09 +0200

    refactor(nvim/multicursors): added description to binds

--- dotfiles/nvim/lua/plugins/motions/multicursors.lua
+++ dotfiles/nvim/lua/plugins/motions/multicursors.lua
@@ -2,38 +2,52 @@ return {
    "jake-stewart/multicursor.nvim",
 
    init = function()
-      local multicursors = require( "multicursor-nvim" );
+      local mc = require( "multicursor-nvim" );
 
-      vim.keymap.set( { "n", "x" }, "<c-k>",         function() multicursors.lineAddCursor( -1 ); end );
-      vim.keymap.set( { "n", "x" }, "<c-j>",         function() multicursors.lineAddCursor( 1 ); end );
-      vim.keymap.set( { "n", "x" }, "<leader><c-k>", function() multicursors.lineSkipCursor( -1 ); end );
-      vim.keymap.set( { "n", "x" }, "<leader><c-j>", function() multicursors.lineSkipCursor( 1 ); end );
+      vim.keymap.set( { "n", "x" }, "<c-k>",
+                      function() mc.lineAddCursor( -1 ); end, { desc = "Multicursor lineAddCursor" } );
+      vim.keymap.set( { "n", "x" }, "<c-j>",
+                      function() mc.lineAddCursor( 1 ); end, { desc = "Multicursor lineAddCursor" } );
+      vim.keymap.set( { "n", "x" }, "<leader><c-k>",
+                      function() mc.lineSkipCursor( -1 ); end, { desc = "Multicursor lineSkipCursor" } );
+      vim.keymap.set( { "n", "x" }, "<leader><c-j>",
+                      function() mc.lineSkipCursor( 1 ); end, { desc = "Multicursor lineSkipCursor" } );
 
-      vim.keymap.set( { "n", "x" }, "<leader>n",     function() multicursors.matchAddCursor( 1 ); end );
-      vim.keymap.set( { "n", "x" }, "<leader>s",     function() multicursors.matchSkipCursor( 1 ); end );
-      vim.keymap.set( { "n", "x" }, "<leader>N",     function() multicursors.matchAddCursor( -1 ); end );
-      vim.keymap.set( { "n", "x" }, "<leader>S",     function() multicursors.matchSkipCursor( -1 ); end );
+      vim.keymap.set( { "n", "x" }, "<leader>n",
+                      function() mc.matchAddCursor( 1 ); end, { desc = "Multicursor matchAddCursor" } );
+      vim.keymap.set( { "n", "x" }, "<leader>s",
+                      function() mc.matchSkipCursor( 1 ); end, { desc = "Multicursor matchSkipCursor" } );
+      vim.keymap.set( { "n", "x" }, "<leader>N",
+                      function() mc.matchAddCursor( -1 ); end, { desc = "Multicursor matchAddCursor" } );
+      vim.keymap.set( { "n", "x" }, "<leader>S",
+                      function() mc.matchSkipCursor( -1 ); end, { desc = "Multicursor matchSkipCursor" } );
 
-      vim.keymap.set( { "n", "x" }, "<c-q>",         multicursors.toggleCursor );
-      vim.keymap.set( { "n", "x" }, "gq",            multicursors.restoreCursors );
-      vim.keymap.set( "x",          "<c-s>",         multicursors.splitCursors );
+      vim.keymap.set( { "n", "x" }, "<c-q>",
+                      mc.toggleCursor, { desc = "Multicursor toggleCursor" } );
+      vim.keymap.set( { "n", "x" }, "gq",
+                      mc.restoreCursors, { desc = "Multicursor restoreCursors" } );
+      vim.keymap.set( "x", "<c-s>",
+                      mc.splitCursors, { desc = "Multicursor splitCursors" } );
 
       -- Mappings defined in a keymap layer only apply when there are
       -- multiple cursors. This lets you have overlapping mappings.
-      multicursors.addKeymapLayer( function( layerSet )
+      mc.addKeymapLayer( function( layerSet )
          -- Select a different cursor as the main one.
-         layerSet( { "n", "x" }, "<c-p>", multicursors.prevCursor );
-         layerSet( { "n", "x" }, "<c-n>", multicursors.nextCursor );
+         layerSet( { "n", "x" }, "<c-p>",
+                   mc.prevCursor, { desc = "Multicursor prevCursor" } );
+         layerSet( { "n", "x" }, "<c-n>",
+                   mc.nextCursor, { desc = "Multicursor nextCursor" } );
 
          -- Delete the main cursor.
-         layerSet( { "n", "x" }, "<c-x>", multicursors.deleteCursor );
+         layerSet( { "n", "x" }, "<c-x>",
+                   mc.deleteCursor, { desc = "Multicursor deleteCursor" } );
 
          -- Enable and clear cursors using escape.
          layerSet( "n", "<esc>", function()
-            if not multicursors.cursorsEnabled() then
-               multicursors.enableCursors();
+            if not mc.cursorsEnabled() then
+               mc.enableCursors();
             else
-               multicursors.clearCursors();
+               mc.clearCursors();
             end;
          end );
       end );
