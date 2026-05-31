e326a1144e4cfb2ad5a7db22b2f518093e294d27
megapixel-code <chainemegapixel@gmail.com>
2026-05-31 16:21:28 +0200

    feat(zsh/aliases): added gs alias to git status

--- dotfiles/zsh/aliases.zsh
+++ dotfiles/zsh/aliases.zsh
@@ -64,7 +64,7 @@ alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
 alias v="nvim"
 alias vim="nvim"
 alias ff="fastfetch"
-alias "git pr"="git pull --rebase"
+alias gs="git status"
 alias lt='tree -a --dirsfirst -I .git/'
 alias python="python3"
 alias t="tmux attach"
