b20ad10c0aa0cc1f0f8991cae8153b5f5e2d3953
megapixel-code <chainemegapixel@gmail.com>
2026-06-26 13:47:56 +0200

    fix(eww/time): time output is now json

--- dotfiles/eww/modules/time.c
+++ dotfiles/eww/modules/time.c
@@ -11,17 +11,6 @@ int main(int argc, char **argv)
    struct tm     *timeinfo;
    struct timeval tv;
 
-   int full = -1;
-   if ( strcmp(argv[1], "partial") ) {
-      full = 1;
-   } else if ( strcmp(argv[1], "full") ) {
-      full = 0;
-   }
-
-   if ( full == -1 ) {
-      return 1;
-   }
-
    while ( 1 ) {
       gettimeofday(&tv, NULL);
       usleep(1000000 - tv.tv_usec);
@@ -67,13 +56,14 @@ int main(int argc, char **argv)
             break;
       };
 
-      if ( full ) {
-         printf("%d %s %d | ",
-                timeinfo->tm_mday,
-                month,
-                timeinfo->tm_year + 1900);
-      }
-      printf("%02d:%02d:%02d\n",
+      printf("{\"full\": \"%d %s %d | %02d:%02d:%02d\"",
+             timeinfo->tm_mday,
+             month,
+             timeinfo->tm_year + 1900,
+             timeinfo->tm_hour,
+             timeinfo->tm_min,
+             timeinfo->tm_sec);
+      printf(", \"partial\": \"%02d:%02d:%02d\" }\n",
              timeinfo->tm_hour,
              timeinfo->tm_min,
              timeinfo->tm_sec);
