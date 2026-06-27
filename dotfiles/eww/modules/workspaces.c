7f6b7e0f9d746bc4b31e35e5a546cb40169e0e2e
megapixel-code <chainemegapixel@gmail.com>
2026-06-27 23:51:58 +0200

    fix(eww/workspaces): mangowc update changed mmsg cmd updated parser

--- dotfiles/eww/modules/workspaces.c
+++ dotfiles/eww/modules/workspaces.c
@@ -9,9 +9,8 @@ FILE *f;
 
 typedef struct {
    char monitor_name[10];
-   int  active_tags[10];
-   int  selected_tags[10];
-   char keymode[10];
+   int  active_tags[9];
+   int  selected_tags[9];
 } monitor_info;
 
 typedef struct {
@@ -28,7 +27,7 @@ void cleanup(int sig)
 
 void open()
 {
-   f = popen("mmsg -w balls -gt", "r");
+   f = popen("mmsg watch all-tags", "r");
 
    if ( f == NULL ) {
       printf("error: could not run the command");
@@ -37,12 +36,25 @@ void open()
    signal(SIGINT, cleanup);
 }
 
-size_t next_whitespace_index(char *buff, size_t index)
+int next_occurrence_end_index(char *buff, char *string, int index)
 {
-   while ( buff[index] != ' ' ) {
+   int offset = 0;
+
+   while ( buff[index + offset] != '\0' && string[offset] != '\0' ) {
+      while ( buff[index + offset] != '\0' &&
+              string[offset] != '\0' &&
+              buff[index + offset] == string[offset] ) {
+         offset++;
+      }
+
+      if ( string[offset] == '\0' ) {
+         return (index + offset - 1);
+      }
+
       index++;
+      offset = 0;
    }
-   return index;
+   return -1;
 }
 
 int same_str(char *buff, char *str)
@@ -53,27 +65,6 @@ int same_str(char *buff, char *str)
       }
    }
    return 1;
-}
-
-int fill_tags(int encoded_val, int *tags)
-{
-   int changed = 0;
-
-   for ( int i = 0; i < 9; i++ ) {
-      if ( (encoded_val >> i) % 2 == 1 ) {
-         if ( tags[i + 1] != 1 ) {
-            changed = 1;
-         }
-         tags[i + 1] = 1;
-      } else {
-         if ( tags[i + 1] != 0 ) {
-            changed = 1;
-         }
-         tags[i + 1] = 0;
-      }
-   }
-
-   return changed;
 }
 
 monitor_info *get_monitor_info(monitor_info_list *monitor_info_list,
@@ -91,7 +82,7 @@ monitor_info *get_monitor_info(monitor_info_list *monitor_info_list,
       monitor_info_list->monitor_info,
       sizeof(*monitor_info_list->monitor_info) * monitor_info_list->size);
 
-   for ( int i = 0; i < 10; i++ ) {
+   for ( int i = 0; i < 9; i++ ) {
       monitor_info_list->monitor_info[monitor_info_list->size - 1]
          .active_tags[i] = 0;
       monitor_info_list->monitor_info[monitor_info_list->size - 1]
@@ -101,8 +92,6 @@ monitor_info *get_monitor_info(monitor_info_list *monitor_info_list,
    strcpy(
       monitor_info_list->monitor_info[monitor_info_list->size - 1].monitor_name,
       monitor_name);
-   strcpy(monitor_info_list->monitor_info[monitor_info_list->size - 1].keymode,
-          "normal");
    return monitor_info_list->monitor_info + (monitor_info_list->size - 1);
 }
 
@@ -117,20 +106,42 @@ void display_monitor_info_json(monitor_info_list monitor_info_list)
    //             "name" : "0"
    //          },
    //          ...
-   //       ],
-   //       "keymode": "default"
+   //       ]
    //    },
    //    "dp1" : ...
    // }
    int first_tag;
+   int selected_tags[10];
+   int tag_zero;
+
    printf("{ ");
    for ( int i = 0; i < monitor_info_list.size; i++ ) {
+      first_tag = 1;
       printf("\"%s\": { \"tags\": [ ",
              monitor_info_list.monitor_info[i].monitor_name);
 
-      first_tag = 1;
-      for ( int j = 0; j < 10; j++ ) {
-         if ( monitor_info_list.monitor_info[i].selected_tags[j] == 0 &&
+      tag_zero = 1;
+      for ( int k = 0; k < 9; k++ ) {
+         if ( monitor_info_list.monitor_info[i].selected_tags[k] == 0 ) {
+            tag_zero = 0;
+            break;
+         }
+      }
+      if ( tag_zero ) {
+         for ( int k = 0; k < 9; k++ ) {
+            selected_tags[k] = 0;
+         }
+         printf("{ \"class\": \"selected_tag\", \"name\": \"0\" }");
+         first_tag = 0;
+      } else {
+         for ( int k = 0; k < 9; k++ ) {
+            selected_tags[k] =
+               monitor_info_list.monitor_info[i].selected_tags[k];
+         }
+      }
+
+      for ( int j = 0; j < 9; j++ ) {
+         if ( selected_tags[j] == 0 &&
               monitor_info_list.monitor_info[i].active_tags[j] == 0 ) {
             continue;
          }
@@ -140,15 +151,14 @@ void display_monitor_info_json(monitor_info_list monitor_info_list)
          }
          first_tag = 0;
          printf("{ \"class\": \"");
-         if ( monitor_info_list.monitor_info[i].selected_tags[j] == 1 ) {
+         if ( selected_tags[j] == 1 ) {
             printf("selected_tag");
          } else if ( monitor_info_list.monitor_info[i].active_tags[j] == 1 ) {
             printf("active_tag");
          }
-         printf("\", \"name\": \"%d\" }", j);
+         printf("\", \"name\": \"%d\" }", j + 1);
       }
-      printf("], \"keymode\": \"%s\" }",
-             monitor_info_list.monitor_info[i].keymode);
+      printf("]}");
 
       if ( i < monitor_info_list.size - 1 ) {
          printf(", ");
@@ -164,79 +174,58 @@ void parser()
 {
    char             *buffer      = NULL;
    size_t            buffer_size = 0;
-   size_t            index;
+   int               index;
    monitor_info_list monitor_info_list = { .size = 0, .monitor_info = NULL };
    monitor_info     *monitor_info;
 
-   // active_tags selected_tags
-   int tags[2];
-   int changed;
+   char *monitor;
+   char *is_selected;
+   int   b_is_selected;
+   int   b_is_active;
+   int   changed;
 
    while ( 1 ) {
       getline(&buffer, &buffer_size, f);
       changed = 0;
+      index   = 0;
 
-      char *monitor = buffer;
-      // skip the monitor to go check directly the second part
-      index         = next_whitespace_index(buffer, 3);
-      buffer[index] = '\0';
-      index++;
-
-      if ( same_str(buffer + index, "keymode") ) {
-         index         = next_whitespace_index(buffer, index);
-         buffer[index] = '\0';
-         index++;
-         char *keymode = buffer + index;
-
-         // we then remove the \n
-         while ( buffer[index] != '\n' ) {
-            index++;
+      while ( 1 ) {
+         index = next_occurrence_end_index(buffer, "\"monitor\":\"", index);
+         if ( index == -1 ) {
+            break;
          }
          buffer[index] = '\0';
+         index++;
+         monitor       = buffer + index;
+         index         = next_occurrence_end_index(buffer, "\"", index);
+         buffer[index] = '\0';
+         index++;
 
          monitor_info = get_monitor_info(&monitor_info_list, monitor);
-         if ( !same_str(monitor_info->keymode, keymode) ) {
-            strcpy(monitor_info->keymode, keymode);
-            changed = 1;
-         }
-      } else if ( same_str(buffer + index, "tags") ) {
-         index         = next_whitespace_index(buffer, index);
-         buffer[index] = '\0';
-         index++;
 
-         char *tags_str;
-         int   tmp_tags_value;
-         for ( int j = 0; j < 2; j++ ) {
-            tags_str      = buffer + index;
-            index         = next_whitespace_index(buffer, index);
+         for ( int i = 0; i < 9; i++ ) {
+            index = next_occurrence_end_index(buffer, "\"is_active\":", index);
+            index++;
+            is_selected   = buffer + index;
+            index         = next_occurrence_end_index(buffer, ",", index);
             buffer[index] = '\0';
             index++;
 
-            tags[j] = atoi(tags_str);
-         }
-
-         monitor_info = get_monitor_info(&monitor_info_list, monitor);
-
-         if ( tags[1] == 511 ) {
-            // on tag 0
-            monitor_info->active_tags[0]   = 1;
-            monitor_info->selected_tags[0] = 1;
-            for ( int i = 1; i < 10; i++ ) {
-               if ( monitor_info->selected_tags[i] != 0 ) {
-                  changed = 1;
-               }
-               monitor_info->selected_tags[i] = 0;
+            b_is_selected = same_str(is_selected, "true");
+            if ( monitor_info->selected_tags[i] != b_is_selected ) {
+               changed                        = 1;
+               monitor_info->selected_tags[i] = b_is_selected;
             }
-         } else {
-            monitor_info->active_tags[0]   = 0;
-            monitor_info->selected_tags[0] = 0;
-            changed =
-               fill_tags(tags[1], monitor_info->selected_tags) || changed;
-         }
-         changed = fill_tags(tags[0], monitor_info->active_tags) || changed;
 
-         // removes the next "tags" section
-         getline(&buffer, &buffer_size, f);
+            index =
+               next_occurrence_end_index(buffer, "\"client_count\":", index);
+            index++;
+            b_is_active = buffer[index] != '0';
+            if ( monitor_info->active_tags[i] != b_is_active ) {
+               changed                      = 1;
+               monitor_info->active_tags[i] = b_is_active;
+            }
+         }
       }
 
       if ( changed ) {
