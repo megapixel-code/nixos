0f7deb68c08249b0eef7032f9f0087402a6dc162
megapixel-code <chainemegapixel@gmail.com>
2026-06-30 17:53:41 +0200

    feat(sops/home-lab): separated home-lab secrets and personal secrets

--- home/secrets.nix
+++ home/secrets.nix
@@ -4,7 +4,8 @@
 }:
 {
   sops = {
-    defaultSopsFile = ../secrets/secrets.yaml;
+    defaultSopsFile =
+      if config.my.module-home-lab.enable then ../secrets/home-lab.yaml else ../secrets/primary.yaml;
     defaultSopsFormat = "yaml";
     age.keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
   };
--- system/secrets.nix
+++ system/secrets.nix
@@ -5,7 +5,12 @@
 }:
 {
   sops = {
-    defaultSopsFile = ../secrets/secrets.yaml;
+    defaultSopsFile =
+      if config.home-manager.users.${user}.my.module-home-lab.enable then
+        ../secrets/home-lab.yaml
+      else
+        ../secrets/primary.yaml;
+
     defaultSopsFormat = "yaml";
     age.keyFile = "${config.home-manager.users.${user}.xdg.configHome}/sops/age/keys.txt";
   };
