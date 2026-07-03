4700d13f19cc8791d3e85e4f41014c651ccf8a12
megapixel-code <chainemegapixel@gmail.com>
2026-07-03 23:28:20 +0200

    fix(eww/battery): battery files were not changed.

--- dotfiles/eww/modules/battery.c
+++ dotfiles/eww/modules/battery.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/inotify.h>
-#include <unistd.h>
-
-#define N_FILE 1
+#include <time.h>
 
 int next_char_index(char *buffer, char c)
 {
    int i = 0;
-   while ( buffer[i] != c ) {
+   while ( buffer[i] != c && buffer[i] != '\0' ) {
       i++;
    }
    return i;
@@ -27,54 +24,36 @@ int same_str(char *buf1, char *buf2)
    return 0;
 }
 
-void add_or_remove_watches(int add, int fd, char **files, int **wd)
-{
-   for ( int i = 0; i < N_FILE; i++ ) {
-      if ( add ) {
-         (*wd)[i] = inotify_add_watch(fd, files[i], IN_CLOSE_NOWRITE);
-         if ( (*wd)[i] == -1 ) {
-            printf("\n");
-            fflush(stdout);
-            exit(0);
-         }
-      } else {
-         inotify_rm_watch(fd, (*wd)[i]);
-      }
-   }
-}
-
 int main()
 {
-   int  *wd            = malloc(sizeof(int) * N_FILE);
-   char *files[N_FILE] = { "/sys/class/power_supply/BAT0/capacity" };
-   struct inotify_event inotify_buffer;
-   int                  i;
-   char                 buffer_status[32];
-   char                 buffer_capacity[4];
+   char  *buffer      = NULL;
+   size_t buffer_size = 0;
 
-   int fd = inotify_init();
-   if ( fd == -1 ) {
-      perror("Could not init inotify");
-      exit(EXIT_FAILURE);
+   time_t time_buf = 0;
+   time_t time_current;
+   int    i;
+   char   buffer_status[32];
+   char   buffer_capacity[4];
+
+   FILE *f = popen("upower -e | upower -m", "r");
+   if ( f == NULL ) {
+      printf("error: cant run cmd\n");
+      exit(1);
    }
-   add_or_remove_watches(1, fd, files, &wd);
 
-   while ( 1 ) {
-      read(fd, &inotify_buffer, sizeof(inotify_buffer));
-      for ( i = 0; i < N_FILE; i++ ) {
-         if ( wd[i] == inotify_buffer.wd ) {
-            break;
-         }
-      }
-      if ( i >= N_FILE ) {
+   while ( getline(&buffer, &buffer_size, f) != -1 ) {
+      time(&time_current);
+      if ( time_current - time_buf < 2 ) {
          continue;
       }
+      time(&time_buf);
 
-      add_or_remove_watches(0, fd, files, &wd);
       FILE *f_capacity = fopen("/sys/class/power_supply/BAT0/capacity", "r");
       FILE *f_status   = fopen("/sys/class/power_supply/BAT0/status", "r");
+
       if ( f_capacity == NULL || f_status == NULL ) {
-         printf("error opening files\n");
+         // printf("error opening files\n");
+         printf("\n");
          exit(1);
       }
 
@@ -83,7 +62,6 @@ int main()
 
       fclose(f_capacity);
       fclose(f_status);
-      add_or_remove_watches(1, fd, files, &wd);
 
       i                  = next_char_index(buffer_capacity, '\n');
       buffer_capacity[i] = '\0';
