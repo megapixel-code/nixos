470a2278e1b86e007f3caa932d4abb366c65d304
megapixel-code <chainemegapixel@gmail.com>
2026-08-11 20:53:15 +0200

    feat(eww/bluetooth): added bluetooth module

---
+++ dotfiles/eww/modules/bluetooth.c
@@ -0,0 +1,57 @@
+#include "library.h"
+
+#include <stddef.h>
+#include <stdio.h>
+#include <unistd.h>
+
+void parser(int *device_count, char *device_name)
+{
+   char  *buffer;
+   size_t buffer_size = 0;
+
+   int   n_devices = 0;
+   char *device;
+   int   index   = 0;
+   int   changed = 0;
+
+   FILE *f = popen("bluetoothctl devices Connected", "r");
+   while ( getline(&buffer, &buffer_size, f) != -1 ) {
+      n_devices++;
+      if ( n_devices > 1 ) {
+         // we don't care about the names, we only want the count
+         continue;
+      }
+
+      lib_get_next_str_char(buffer, &index, ' ');
+      lib_get_next_str_char(buffer, &index, ' ');
+      device = lib_get_next_str_char(buffer, &index, '\n');
+
+      changed = assign(device, device_name) || changed;
+   }
+   fclose(f);
+
+   changed = assign(&n_devices, device_count) || changed;
+   if ( !changed ) {
+      return;
+   }
+
+   printf("bt: ");
+   if ( n_devices == 0 ) {
+      printf("none\n");
+   } else if ( n_devices == 1 ) {
+      printf("%s\n", device);
+   } else {
+      printf("%d devices\n", n_devices);
+   }
+   fflush(stdout);
+}
+
+int main(int argc, char *argv[])
+{
+   int  device_count = -1;
+   char device_name[64];
+   while ( 1 ) {
+      parser(&device_count, device_name);
+      sleep(2);
+   }
+}
